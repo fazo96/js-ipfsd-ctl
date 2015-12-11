bb5ca9d34ad55b09e271efb5639b0f540bb20fa3
Enrico Fasoli <fazius2009@gmail.com>
2015-12-11 19:42:14 +0100

    wip fixing options

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ module.exports = {
       done = opts
       opts = {}
     }
-    opts['Addresses.Swarm'] = ['/ip4/0.0.0.0/tcp/0']
-    opts['Addresses.Gateway'] = ''
-    opts['Addresses.API'] = '/ip4/127.0.0.1/tcp/0'
+    //opts['Addresses.Swarm'] = ['/ip4/0.0.0.0/tcp/0']
+    //opts['Addresses.Gateway'] = ''
+    //opts['Addresses.API'] = '/ip4/127.0.0.1/tcp/0'
     const node = new Node(tempDir(), opts, true)
     node.init(err => {
       if (err) return done(err)
